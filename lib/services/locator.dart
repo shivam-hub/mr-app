e4249362eeedbcabdf1a04f4b6020f6ae12149bf
Shivam Pandey <shivampandey4194@gmail.com>
2023-12-19 23:43:41 +0530

    integrated login api

---
+++ lib/services/locator.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+import 'package:nurene_app/utils/const.dart';
+import '../services/api_services.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => ApiService(Constants.baseurl));
+  // Add other services as needed
+}
